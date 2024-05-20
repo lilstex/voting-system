a1d6c45bd9171e01b7ac07958c96908323111e5f
Emmanuel C Mbagwu <lilstex4good@gmail.com>
2024-05-20 20:17:20 +0100

    Updated the voting system contarct to adhere to the natural specification documentation guidelines for contract

--- contracts/VoteSystem.sol
+++ contracts/VoteSystem.sol
@@ -3,13 +3,26 @@ pragma solidity ^0.8.19;
 
 error NotOwner();
 
+/**
+ *  @title A voting system
+ * @author Emmanuel Mbagwu
+ * @notice You can use this contract for simple voting exercises
+ */
 contract VoteSystem {
+    /**
+     *  @title Candidate object
+     * @notice Holds the details of a candidate
+     */
     struct Candidate {
         uint256 id;
         string name;
         uint256 voteCount;
     }
 
+    /**
+     *  @title Voter object
+     * @notice Holds the details of a voter
+     */
     struct Voter {
         bool authorised;
         bool voted;
@@ -26,12 +39,18 @@ contract VoteSystem {
     mapping(uint256 => Candidate) public candidates;
     mapping(address => Voter) public voters;
 
+    /**
+     * @notice An event that has the result of the voting
+     */
     event VoteResult(
         uint256 candidateId,
         string candidateName,
         uint256 voteCount
     );
 
+    /**
+     * @notice A constructor that initializes the contract with a name for the election
+     */
     constructor(string memory _name) {
         i_owner = msg.sender;
         electionName = _name;
@@ -42,40 +61,50 @@ contract VoteSystem {
         _;
     }
 
-    // Function to add candidate for the election
+    /**
+     * @notice Adds a candidate to the candidates list for the election
+     * @dev Increments the number of candidates in the candidate list
+     */
     function addCandidate(string memory _name) public onlyOwner {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    // Function to authorize voters for the voting process
+    /**
+     * @notice Authorizes voters for the voting exercise
+     * @dev Increments the number of authorized voters
+     */
     function authorizeVoter(address _person) public onlyOwner {
-        require(!votingEnded, "Voting has already ended."); // Check if voting has ended
+        require(!votingEnded, "Voting has already ended.");
         totalVoters++;
         voters[_person].authorised = true;
     }
 
-    // Function to cast votes by authorised voters
+    /**
+     * @notice Authorised voters are allowed to cast their votes
+     * @dev Increments the candidates vote count, and marks voter to have voted
+     */
     function vote(uint256 candidateId) public {
         require(
             !voters[msg.sender].authorised,
             "You are not authorised to vote"
-        ); // Check if voter is authorised
-        require(!votingEnded, "Voting has already ended."); // Check if voting has ended
-        require(voters[msg.sender].voted, "You already voted"); // Check if voter already voted
+        );
+        require(!votingEnded, "Voting has already ended.");
+        require(voters[msg.sender].voted, "You already voted");
         voters[msg.sender].voted = true;
         voters[msg.sender].vote = candidateId;
 
-        // Update candidates vote
         candidates[candidateId].voteCount += 1;
-        // Update total votes cast
         totalVotes += 1;
     }
 
-    // Function to end election and announce results
+    /**
+     * @notice A function to end voting
+     * @dev Loops through the candidates list and emits their results
+     */
     function end() public onlyOwner {
         require(!votingEnded, "Voting has ended.");
-        // Loop through the candidates to emit the election result
+
         for (uint256 i = 1; i <= candidatesCount; i++) {
             emit VoteResult(
                 candidates[i].id,
